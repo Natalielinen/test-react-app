cfa8cd2a4247e4e1abba3bc225df4a77166d3de3
Natalie <nataliech0409@gmail.com>
2020-09-29 14:50:11 +0300

    Nav Link and refactoring

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import classes from './Messages.module.css';
 
 
 const Messages = () => {
     return (
-        <div>Messages</div>
+        <div className={classes.dialogs}>
+            <div className={classes.usersList}>
+                <div className={classes.user}>Гарик</div>
+                <div className={classes.user}>Соня</div>
+                <div className={classes.user}>Белка</div>
+                <div className={classes.user}>Дуся</div>
+                <div className={classes.user}>Василий</div>
+                <div className={classes.user}>Джон</div>
+                <div className={classes.user}>Карась</div>
+            </div>
+            <div className={classes.items}>
+                <div className={classes.item}>Мяу!!!</div>
+                <div className={classes.item}>Гав-гав!!!</div>
+                <div className={classes.item}>*звуки улитки*</div>
+            </div>
+        </div>
     )
 }
 
